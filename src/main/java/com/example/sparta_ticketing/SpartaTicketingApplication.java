4042605f0dcd719eb51211277f64ff57118cc756
khw00185 <ksng0185@naver.com>
2025-03-24 18:09:00 +0900

    chore: initial commit

---
+++ src/main/java/com/example/sparta_ticketing/SpartaTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.example.sparta_ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaTicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaTicketingApplication.class, args);
+    }
+
+}

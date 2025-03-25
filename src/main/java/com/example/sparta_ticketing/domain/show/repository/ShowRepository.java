abfb03355526e2b9fc39f465b8c81a77945c717f
abc <clvlajrrhtlvek@gmail.com>
2025-03-25 21:09:53 +0900

    fix(show): 공연 조회 수정
    삭제된 공연의 경우 조회되지 않도록 수정하였습니다.

--- src/main/java/com/example/sparta_ticketing/domain/show/repository/ShowRepository.java
+++ src/main/java/com/example/sparta_ticketing/domain/show/repository/ShowRepository.java
@@ -2,7 +2,13 @@ package com.example.sparta_ticketing.domain.show.repository;
 
 import com.example.sparta_ticketing.domain.show.entity.Show;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ShowRepository extends JpaRepository<Show, Long> {
 
+    @Query("SELECT s FROM Show s WHERE s.id = :showId AND s.status = 'NOT_DELETED'")
+    Optional<Show> findShowById(@Param("showId") Long showId);
 }
